1335d2bb7c6ff0c245a6f90d56d8f5cb071be3d9
princedan <princedan@163.com>
2014-04-15 09:58:12 +0800

    t1

---
+++ t1Tests/T1.h
@@ -0,0 +1,13 @@
+//
+//  T1.h
+//  t1
+//
+//  Created by huadoo on 14-4-15.
+//  Copyright (c) 2014年 com.huadoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface T1 : NSObject
+
+@end
---
+++ t1Tests/T1.m
@@ -0,0 +1,13 @@
+//
+//  T1.m
+//  t1
+//
+//  Created by huadoo on 14-4-15.
+//  Copyright (c) 2014年 com.huadoo. All rights reserved.
+//
+
+#import "T1.h"
+
+@implementation T1
+
+@end
